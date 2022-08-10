c013960c7167c81b6f18e6ccf43f0faf6c7388e2
ovara1mark <markovara1@gmail.com>
2022-08-10 23:53:58 +0100

    post request update

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,20 +1,65 @@
-import os
-from flask import Flask 
-# request, abort, jsonify
-
-# from flask_cors import CORS
-
+import os 
+from flask import Flask, request, abort, jsonify
 
 from models import setup_db, Question
 
-def create_app(test_config=None):
-    # create and configure the app
+def create_app(test_config = None ):
     app = Flask(__name__)
     setup_db(app)
 
+    @app.route("/questions", methods=["POST"])
+    def post_questions():
+        add = request.get_json()
+
+        new_answer = add.get("answer")
+        new_difficulty = add.get("difficulty")
+        new_category = add.get("category")
+        new_question = add.get("question")
+
+
+        try:
+            questions = Question(answer=new_answer, 
+                                 question=new_question,
+                                 category = new_category,
+                                 difficulty = new_difficulty   
+                                )
+            questions.insert()
+
+            return jsonify({
+                "success": "True",
+                "questions_post":questions.format()
+            })
+
+        except:
+            abort(405)
+
     @app.route("/")
     def index():
-        return "hello world"
+        return "hello woke"
 
+    @app.errorhandler(404)
+    def not_found(error):
+        return (
+            jsonify({"success": False, "error": 404, "message": "resource not found"}),
+            404,
+        )
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return (
+            jsonify({"success": False, "error": 422, "message": "unprocessable"}),
+            422,
+        )
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({"success": False, "error": 400, "message": "bad request"}), 400
+
+    @app.errorhandler(405)
+    def not_found(error):
+        return (
+            jsonify({"success": False, "error": 405, "message": "method not allowed"}),
+            405,
+        )
 
     return app
--- models.py
+++ models.py
@@ -1,55 +1,50 @@
 import os
-from sqlalchemy import Column, String, Integer, create_engine
-from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
+from flask_sqlalchemy import SQLAlchemy 
 
-database_name = 'century'
+database_name = "century"
 database_path ="postgresql://{}:{}@{}/{}".format('postgres', 'ghostman','localhost:5432', database_name)
 
 db = SQLAlchemy()
 
-"""
-setup_db(app)
-    binds a flask application and a SQLAlchemy service
-"""
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False 
     db.app = app
     db.init_app(app)
     db.create_all()
 
 
 class Question(db.Model):
-    __tablename__ = 'questions'
-
+    __tablename__ = "questions"
     id = Column(Integer, primary_key=True)
-    question = Column(String)
     answer = Column(String)
     category = Column(String)
+    question = Column(String)
     difficulty = Column(Integer)
 
-    def __init__(self, question, answer, category, difficulty):
-        self.question = question
+    def __init__(self, answer, category, difficulty, question):
         self.answer = answer
+        self.question = question
         self.category = category
         self.difficulty = difficulty
 
     def insert(self):
-        db.session.add(self)
-        db.session.commit()
+        db.session_add(self)
+        db.session_commit()
 
     def update(self):
-        db.session.commit()
+        db.session_commit()
 
     def delete(self):
-        db.session.delete(self)
-        db.session.commit()
+        db.session_delete(self)
+        db.session_commit()
 
     def format(self):
         return {
-            'id': self.id,
-            'question': self.question,
-            'answer': self.answer,
-            'category': self.category,
-            'difficulty': self.difficulty
-            }
+            "id": self.id,
+            "answer": self.answer,
+            "question": self.question,
+            "category": self.category,
+            "difficulty": self.difficulty
+        }
